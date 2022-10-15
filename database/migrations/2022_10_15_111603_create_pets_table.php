2eb7141dd73386ba2949b194b10f0f37e984f389
Jnarrr <lfebruary60@gmail.com>
2022-10-15 19:36:00 +0800

    petModelMigrationControllerAndAPI

---
+++ database/migrations/2022_10_15_111603_create_pets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+            $table->string('pet_name', 50);
+            $table->string('pet_type', 20);
+            $table->string('pet_sex', 10);
+            $table->string('pet_breed', 20);
+            $table->string('pet_birthdate', 20);
+            $table->string('pet_weight', 20);
+            $table->string('pet_description', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pets');
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\ClinicController;
 use App\Http\Controllers\VetController;
+use App\Http\Controllers\API\PetController;
 
 Route::get('clinics', [ClinicController::class, 'index']);
 Route::post('/add-clinic', [ClinicController::class, 'store']);
@@ -12,3 +13,5 @@ Route::put('update-clinic/{id}', [ClinicController::class, 'update']);
 Route::delete('delete-clinic/{id}', [ClinicController::class, 'destroy']);
 
 Route::post('vetregister', [VetController::class, 'vetregister']);
+
+Route::get('pets', [PetController::class, 'index']);
