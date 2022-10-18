465a35629a85e7977fd4f27c35ac4c1b6a769f7e
Jnarrr <lfebruary60@gmail.com>
2022-10-19 00:40:03 +0800

    AppointmentAPIforReactNative

---
+++ app/Http/Controllers/API/AppointmentController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Appointment;
+use Illuminate\Http\Request;
+
+class AppointmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $appointment = Appointment::all();
+
+        $data = [
+            'status' => true,
+            'appointments' => $appointment
+        ];
+
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $appointment = new Appointment;
+
+        $appointment->procedure = $request->procedure;
+        $appointment->date = $request->date;
+        $appointment->time = $request->time;
+        $appointment->pet = $request->pet;
+
+        $appointment->save();
+
+        $data = [
+            'status' => true,
+            'appointment' => $appointment
+        ];
+
+        return response()->json($data, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Appointment  $appointment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Appointment $appointment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Appointment  $appointment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Appointment $appointment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Appointment  $appointment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Appointment $appointment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Appointment  $appointment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Appointment $appointment)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\ClinicController;
 use App\Http\Controllers\VetController;
 use App\Http\Controllers\API\PetController;
+use App\Http\Controllers\API\AppointmentController;
 
 Route::get('clinics', [ClinicController::class, 'index']);
 Route::post('/add-clinic', [ClinicController::class, 'store']);
@@ -17,3 +18,6 @@ Route::post('vetlogin', [VetController::class, 'vetlogin']);
 
 Route::get('pets', [PetController::class, 'index']);
 Route::post('pets', [PetController::class, 'store']);
+
+Route::get('appointments', [AppointmentController::class, 'index']);
+Route::post('appointments', [AppointmentController::class, 'store']);
